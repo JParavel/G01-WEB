723338c167ae3918629e234a56735428be69aa30
Paravel <jgamezc@uninorte.edu.co>
2022-11-24 09:02:02 -0500

    Primeras Rutas

--- MiAppWebG01/app.js
+++ MiAppWebG01/app.js
@@ -3,8 +3,21 @@ import express from "express";
 const app = express();
 const puerto = 3000;
 
-function callback(){
+app.listen(puerto, () => {
     console.log("El servidor se esta ejecutando.");
-}
+})
 
-app.listen(puerto, callback)
+app.get("/", (req, res) => {
+    //Implementar el codigo
+    res.send("Hola, Mundo!")
+})
+
+app.get("/home", (req, res) => {
+    //Implementar el codigo
+    res.send("Estas en el apartado HOME")
+})
+
+app.get("/home/G01", (req, res) => {
+    //Implementar el codigo
+    res.send("Bienvenido a la clase G01")
+})
