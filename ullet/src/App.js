8cd38ff16a67c45c467172db72b7fcbf0d8cac06
JParavel <jgamezc@uninorte.edu.co>
2022-12-06 09:06:26 -0500

    Componentes en React

---
+++ ullet/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './components/forms/Button';
+import "./assets/global.css"
+import Login from './components/Login';
+
+
+export default function App() {
+    return (<Login></Login>)
+}
